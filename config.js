a7523b3e281cde4588d1cf660dcdba36c4fd0888
amandeeptoor2804-rgb <amandeeptoor2804@gmail.com>
2025-10-24 14:27:58 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
-// ✅ Firebase config for Trading Journal (Anonymous Sign-in)
-
-export const appId = "trading-journal-da4eb";
-
+// config.js
 export const firebaseConfig = {
   apiKey: "AIzaSyB5rvkKPst6V9-7HoqK1f27jQj6x7f8LiA",
   authDomain: "trading-journal-da4eb.firebaseapp.com",
   projectId: "trading-journal-da4eb",
-  storageBucket: "trading-journal-da4eb.appspot.com", // ✅ correct bucket
+  storageBucket: "trading-journal-da4eb.firebasestorage.app",
   messagingSenderId: "50906470929",
   appId: "1:50906470929:web:b9ad784e9ae86c06d42fb0",
   measurementId: "G-WR261BKTXL"
