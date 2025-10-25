59c4e77178f4f7f3ba608febc9bd6798a7bd31d9
amandeeptoor2804-rgb <amandeeptoor2804@gmail.com>
2025-10-24 23:48:21 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,36 @@
-// Simple offline cache (same-origin files)
-const CACHE = "tj-v1";
-const CORE_ASSETS = [
+const CACHE = "tj-v3";
+const ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json"
-  // icons and other same-origin files can be added here if you like:
-  // "./icons/icon-192.png",
-  // "./icons/icon-512.png"
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE_ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch", e => {
   const req = e.request;
-  // Only cache same-origin GET requests
-  if (req.method !== "GET" || new URL(req.url).origin !== location.origin) return;
+  if (req.method !== "GET") return;
   e.respondWith(
-    caches.match(req).then(
-      (cached) =>
-        cached ||
-        fetch(req)
-          .then((res) => {
-            const copy = res.clone();
-            caches.open(CACHE).then((c) => c.put(req, copy));
-            return res;
-          })
-          .catch(() => cached)
+    caches.match(req).then(cached =>
+      cached ||
+      fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return res;
+      }).catch(() => cached)
     )
   );
 });
