ca36d1b7a16d3f97fefcc473b3c497f2f11e3b25
amandeeptoor2804-rgb <amandeeptoor2804@gmail.com>
2025-10-24 19:21:06 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,9 @@
-// Simple offline-first cache
-const CACHE_NAME = "tj-cache-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate", (e) => {
+  clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((cached) => cached || fetch(event.request))
-      .catch(() => caches.match("./index.html"))
-  );
-});
+self.addEventListener("fetch", () => {});
