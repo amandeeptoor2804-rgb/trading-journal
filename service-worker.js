95c9bd7042f23d6526680d03dd88d11dbe00306a
amandeeptoor2804-rgb <amandeeptoor2804@gmail.com>
2025-10-24 21:47:37 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,33 @@
-self.addEventListener("install", (e) => {
+const CACHE = 'tj-cache-v7';
+const ASSETS = [
+  '/', '/index.html',
+  '/manifest.json?v=7',
+  '/assets/icons/favicon-16.png?v=7',
+  '/assets/icons/favicon-32.png?v=7',
+  '/assets/icons/apple-touch-icon.png?v=7',
+  '/assets/icons/app-192.png?v=7',
+  '/assets/icons/app-512.png?v=7'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-  clients.claim();
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(cached => cached || fetch(e.request))
+    );
+  }
+});
